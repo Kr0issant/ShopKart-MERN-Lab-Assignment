713a76a9c6c17229f4094a9d703bc8f80cf1bddf
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-09-17 01:55:53 +0530

    define get products (all and by id)

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -4,12 +4,9 @@ export async function addProduct(req, res) {
     try {
         const { name, description, price, category, image, stock } = req.body;
 
-        if (!name || !description || !price || !category || !image) {
-            return res.status(400).json({
-                message: "All fields are required"
-            });
+        if (!name || !description || !price || !category || !image || !stock) {
+            return res.status(400).json({ message: "All fields are required" });
         }
-
         if (stock < 0) {
             return res.status(400).json({ success: false, message: "Stock must not be negative" });
         }
@@ -23,6 +20,29 @@ export async function addProduct(req, res) {
 
         return res.status(201).json({ success: true, message: "Product added successfully", product });
     } catch (error) {
+        return res.status(500).json({ success: false, message: "Internal server error", error: error });
+    }
+}
+
+export async function getProducts(req, res) {
+    try {
+        const products = await Product.find({});
+        return res.status(200).json({ success: true, count: products.length, products: products });
+    } catch(error) {
+        return res.status(500).json({ success: false, message: "Internal server error", error: error });
+    }
+}
+
+export async function getProduct(req, res) {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.status(404).json({ success: false, message: "Product not found" });
+        }
+
+        return res.status(200).json({ success: true, product: product });
+    } catch(error) {
         return res.status(500).json({ success: false, message: "Internal server error", error: error });
     }
 }
