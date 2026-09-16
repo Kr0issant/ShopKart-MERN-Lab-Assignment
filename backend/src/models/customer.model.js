7358b6418b3bd11acd2df3fb039f3580c213cdad
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2026-09-16 20:27:35 +0530

    add customer model

---
+++ backend/src/models/customer.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const customerSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    },
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+export default Customer;
